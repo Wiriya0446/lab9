4b85609d8544a46004378742ad25422efeb007b5
Wiriya0446 <wiriyakhotchahin@gmail.com>
2023-12-15 17:27:34 +0700

    finish

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
-
+double loan,rate,Payment,Interest,NewBalance;
+int EndOfYear=1;
 int main(){	
 	cout << "Enter initial loan: ";
+	cin>>loan;
 	cout << "Enter interest rate per year (%): ";
+	cin>>rate;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	cin>>Payment;
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +17,25 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	NewBalance = loan ;
+	int end = 1 ;
+	while(end != 0){
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << EndOfYear;//round
+		cout << setw(13) << left << NewBalance;//PrevBalance
+		Interest=NewBalance*(rate/100);
+		cout << setw(13) << left << Interest;//Interest
+		cout << setw(13) << left << NewBalance+Interest;//Total
+		if(NewBalance+Interest<Payment){
+			Payment=loan+Interest;
+			end = 0 ;
+		}
+		cout << setw(13) << left << Payment;//Payment
+		NewBalance=(loan+Interest)-Payment;
+		
+		cout << setw(13) << left << NewBalance<<"\n";//NewBalance
+		EndOfYear++;
+		loan  = NewBalance ;
+	}
 	return 0;
 }
